6a5c110ab645b870b1de53666039db015d9929bd
Danica Dombek <danica.dombek@gmx.de>
2021-05-26 15:49:37 +0200

    Make templates for stories, component and test

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const writeFile = require('./writeFile')
 const inquirer = require('inquirer')
 
+const templates = {
+    component: name => `import styled from 'styled-components/macro'
+
+    export default function ${name} () {
+        return <div>${name}</div>
+    }
+    `,
+
+    spec: name => `
+    import {render, screen} from '@testing-library/react'
+    import ${name} from './${name}'
+
+    describe('${name}', () => {
+        it('renders', () => {
+            render(<${name}/>)
+            expect(screen.getByText('${name}')).toBeInTheDocument()
+        })
+    })
+    `
+
+    stories: name => `
+    import ${name} from './${name}'
+
+    export default {
+        title: '${name}',
+        component: ${name}
+    }
+
+    const Template = args => <${name} {...args}/>
+
+    export const Default = Template.bind({})
+    Default.args = {}
+    `,
+}
+
 const questions = [
   {
     type: 'input',
